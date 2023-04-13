47dd48ce604c8295cc67aacc8ceb1d1fc4508983
KabirYusuf <sonkaybee@gmail.com>
2023-04-13 14:06:41 +0100

    variable name changed

--- src/chapter4/LargestNumber.java
+++ src/chapter4/LargestNumber.java
@@ -6,13 +6,13 @@ public class LargestNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number");
-        int num = scanner.nextInt();
-        int maxNum = num;
-        for (int i = 0; i < 9; i++) {
+        int number = scanner.nextInt();
+        int largest = number;
+        for (int counter = 0; counter < 9; counter++) {
             System.out.println("Enter a number");
-            num = scanner.nextInt();
-            if (num > maxNum)maxNum = num;
+            number = scanner.nextInt();
+            if (number > largest) largest = number;
         }
-        System.out.printf("Largest Number is: %d", maxNum);
+        System.out.printf("Largest Number is: %d", largest);
     }
 }
