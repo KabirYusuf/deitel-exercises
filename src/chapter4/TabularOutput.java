7122f29ce870996c08590ce484a097ae5efd65da
KabirYusuf <sonkaybee@gmail.com>
2023-04-13 14:28:57 +0100

    Tabular output completed

--- src/chapter4/TabularOutput.java
+++ src/chapter4/TabularOutput.java
@@ -1,4 +1,19 @@
 package chapter4;
 
 public class TabularOutput {
+    public static void main(String[] args) {
+
+        System.out.println("N\tN2\tN3\tN4");
+        for (int i = 1; i <= 5; i++) {
+            int num = i;
+            System.out.print(i);
+            System.out.print("\t");
+            for (int j = 1; j < 4; j++) {
+                num *= i;
+                System.out.print(num);
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
 }
